27850cf31bfa94da7cccdf8f12463848e01a8daf
maxim <94103983+Maksmaximusmaxim@users.noreply.github.com>
2022-07-03 11:10:53 +0300

    k

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -3,7 +3,7 @@ export const FriendList = ({friends})=>
     <ul >
  {friends.map(friend =>
      <li key={friend.id} className={css.item}> 
-     <span className="status" ></span> 
+     <span className={friend.isOnline ? css.on : css.off} ></span> 
     <img className="avatar" src={friend.avatar} alt="User avatar" width="48" /> 
      <p className={css.name}>{friend.name}</p> 
    </li> )}
